35c26fe1ba38f6c631141c34b99c7c2c158a5d21
AryanBagoria <70791052+AryanBagoria@users.noreply.github.com>
2021-11-16 00:06:04 +0530

    Add files via upload

---
+++ morseblink.py
@@ -0,0 +1,81 @@
+from guizero import App, Text, TextBox, PushButton
+import RPi.GPIO as GPIO
+import time
+
+PIN = 22
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN, GPIO.OUT)
+
+letters = {
+      'A': '.-', 
+      'B': '-...', 
+      'C': '-.-.', 
+      'D': '-..', 
+      'E': '.', 
+      'F': '..-.', 
+      'G': '--.', 
+      'H': '....', 
+      'I': '..', 
+      'J': '.---', 
+      'K': '-.-', 
+      'L': '.-..', 
+      'M': '--', 
+      'N': '-.', 
+      'O': '---', 
+      'P': '.--.', 
+      'Q': '--.-', 
+      'R': '.-.', 
+      'S': '...', 
+      'T': '-', 
+      'U': '..-', 
+      'V': '...-', 
+      'W': '.--', 
+      'X': '-..-', 
+      'Y': '-.--', 
+      'Z': '--..', 
+      '1': '.----', 
+      '2': '..---', 
+      '3': '...--', 
+      '4': '....-', 
+      '5': '.....', 
+      '6': '-....', 
+      '7': '--...', 
+      '8': '---..', 
+      '9': '----.', 
+      '0': '-----', 
+      ' ': '/'
+          }
+
+def led_control(time_sleep):
+    GPIO.output(PIN, GPIO.HIGH)
+    time.sleep(time_sleep)
+    GPIO.output(PIN, GPIO.LOW)
+    time.sleep(0.5)
+
+def led_blink():
+    appMessage.value = inputText.value
+    if len(appMessage. value) > 12:
+        appMessage.value = "Cannot enter more than 12 characters"
+    else:
+        appMessage.value = appMessage.value + " "
+        word = inputText.value
+        word = word.upper ()
+        for letter in word:
+                time.sleep(2)
+                morseCodeLetter = letters[letter]
+                for codes in morseCodeLetter:
+                    if codes ==  "_":
+                        led_control(0.5)
+                    elif codes == ".":
+                        led_control (0.2)
+                    elif codes == "/":
+                        time.sleep(1)
+
+app = App(title = "Morse Code")
+appMessage = Text(app,text = "Enter text to get morse code signal" ,size = 15, color="BLACK")
+inputText = TextBox(app, width=20)
+ledBlink = PushButton(app, command=led_blink, text ="BLINK LED" ,width = 20)
+app.display()
+
+GPIO.cleanup()
